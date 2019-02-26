690e7b913751ea5b61e32af228ed88456e3f7985
TaiNT <tai.nguyentrung1996@gmail.com>
2019-02-27 01:32:54 +0700

    Done comment in Post. Now doing Page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 8 }
   validates :body, presence: true
 end
