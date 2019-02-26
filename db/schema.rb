76100322eb1bcc609a7495b6fc9e4d8a776098ed
TaiNT <tai.nguyentrung1996@gmail.com>
2019-02-27 01:22:03 +0700

    Doing Comment to Post of Tumbler App

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_150658) do
+ActiveRecord::Schema.define(version: 2019_02_26_180030) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
